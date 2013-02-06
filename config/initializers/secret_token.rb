f18eebc7b1a6dd3471736caf19571fcd5b1efe68
Morgan Lang <morgan@9mmedia.com>
2013-02-05 19:39:10 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Artster::Application.config.secret_token = '4a3bf999d8b093ea13c04125998052df24d7edca34fa238162cfd1c0afd89e1754052dc9cc04830bec42f113448d6795cefe6757f19afb5e42eee14446fe2035'
